88b7345cd5f650cde9b18bf80a67bb87334e6289
Abbey A <abigailjallison@gmail.com>
2025-12-26 10:22:30 -0600

    Simplified weekly overview variable.

--- scripts/01_wrangling.R
+++ scripts/01_wrangling.R
@@ -2,15 +2,14 @@
 
 # 1. Epi Curve Overview
 # weekly cases
-grouped_overview <- covid_clean |>
+weekly_overview <- covid_clean |>
   filter(!is.na(pos_sampledt)) |> # n=122 missing positive sample dates
   mutate(week_start = lubridate::floor_date(pos_sampledt, "weeks", week_start = 1)) |>
-  count(week_start, name = "Number of Cases")
+  count(week_start, name = "Number of Cases") |> 
+  mutate(cdc_week = epiweek(week_start)) |> 
+  mutate(label = paste0("CDC Week ", cdc_week))
 
-grouped_overview$cdc_week <- epiweek(grouped_overview$week_start)
-grouped_overview$label = paste0("CDC Week ", grouped_overview$cdc_week)
-
-# 2. Cases
+# 2. Race/Demographics
 cases_demo <- covid_clean |> 
   group_by(case_race) |> 
   summarize(Count=n()) |> 
@@ -23,3 +22,10 @@ covid_deaths <- covid_clean |>
   filter(!is.na(pos_sampledt)) |>
   mutate(week_start = lubridate::floor_date(pos_sampledt, "weeks", week_start = 1)) |>
   count(week_start, name = "Number of Deaths")
+
+weekly_overview <- weekly_overview |>
+  left_join(covid_deaths, by = "week_start") |>
+  mutate(
+    # replace NA with 0
+    `Number of Deaths` = tidyr::replace_na(`Number of Deaths`, 0L),
+    death_rate = `Number of Deaths` / `Number of Cases`)
